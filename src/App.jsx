d2bed46142871a0f92ea2c9f1102d2d224e7cb0f
Turbash Negi <negirawatdeepi@gmail.com>
2025-03-05 15:04:21 +0530

    few change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Counter from './components/Button.jsx'
+import Button from './components/Button.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,9 +10,9 @@ function App() {
   return (
     <>
       <div className='h-[100%] w-[100%] flex justify-center items-center'>
-        <Counter updateCount={updateCount}>
+        <Button updateCount={updateCount}>
           <p>You have clicked this button {count} times</p>
-        </Counter>
+        </Button>
       </div>
     </>
   )
